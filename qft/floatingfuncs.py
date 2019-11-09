0a27ee51aec464a219ddb566e0620dc4785cb433
lennonokun <lennonokun@gmail.com>
2019-11-08 19:58:27 -0500

    added Bond subclasses, tests, and example

--- qft/floatingfuncs.py
+++
@@ -1,7 +0,0 @@
-def bootstrap_rate(fv, pmt, nper, s_n):
-    return ((pmt + fv)/fv*(1+s_n)**nper)**(1/(nper+1)) - 1
-
-
-def floating_pv(fv, pmt, spots):
-    return sum(pmt/(1+r)**(n+1) for n, r in enumerate(spots)) \
-           + fv/(1+spots[-1])**len(spots)
---
+++ tests/testbond.py
@@ -0,0 +1,10 @@
+from qft.qft import solve_spot, floating_pv, bootstrap
+
+fv = 100
+pmt = 10
+rate = 0.10
+
+rates = [0.10]
+
+print(bootstrap(fv, pmt, 10))
+
